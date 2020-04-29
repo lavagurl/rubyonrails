89a8ef0017b42a3dcefc504881c4345f0a475c5b
lavagurl <51229328+lavagurl@users.noreply.github.com>
2020-04-29 11:59:04 +0200

    Update exercice1.rb

--- exercice1.rb
+++ exercice1.rb
@@ -4,10 +4,12 @@ ch1 = gets.chomp!
 puts "Donner une deuxième chaine de caractère :"
 ch2 = gets.chomp!
 
-if ch1.length*2 > ch2.length
+if ch1.length == ch2.length*2
   puts "La première chaine : \"#{ch1}\", est 2 fois grande que la deuxième : \"#{ch2}\""
 else
-  if ch2.length*2 > ch1.length
+  if ch2.length == ch1.length*2
     puts "La deuxième chaine : \"#{ch2}\", est 2 fois grande que la première : \"#{ch1}\""
+  else 
+    puts "Aucune chaine n'est le double de l'autre"
   end
 end
